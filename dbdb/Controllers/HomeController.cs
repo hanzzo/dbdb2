063af7823ae7ce88e6a20d699d9074eb737d4bad
Honza Moravec <jan.moravec@accenture.com>
2016-11-29 16:44:15 +0100

    specific 02

--- dbdb/Controllers/HomeController.cs
+++ dbdb/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace dbdb.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "APP";
+            ViewBag.Title = "customer 02";
 
             return View();
         }
